3c677d02409a5398aa4269b1f37a3d3e06381ea9
Kalpazan <kalpazang@gmail.com>
2013-04-14 14:53:03 +0300

    bootstrap

--- src/controller/ControllerServlet.java
+++ src/controller/ControllerServlet.java
@@ -12,17 +12,16 @@ import java.util.List;
 public class ControllerServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String temp = new String(request.getParameter("word").getBytes("ISO-8859-1"),"UTF-8");
+        String temp = new String(request.getParameter("word").getBytes("ISO-8859-1"), "UTF-8");
         char[] word = temp.toCharArray();
-//        System.out.println(new String(request.getParameter("word").getBytes("ISO-8859-1"),"UTF-8"));
-        List<Character> vowels = Arrays.asList('а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е');
-        for (int i = 0; i < word.length-1; i++) {
-            if (vowels.contains(word[i])){
+        List<Character> vowels = Arrays.asList('А', 'а', 'У', 'у', 'О', 'о', 'Ы', 'ы', 'И', 'и', 'Э', 'э', 'Я', 'я', 'Ю', 'ю', 'Ё', 'ё', 'Е', 'е');
+        for (int i = 0; i < word.length - 1; i++) {
+            if (vowels.contains(word[i])) {
                 word[i] = 'i';
             }
         }
         System.out.println(word);
-        request.setAttribute("wird", word);
+        request.setAttribute("wird", new String(word));
         RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
         dispatcher.forward(request, response);
     }
--- web/index.jsp
+++ web/index.jsp
@@ -2,12 +2,15 @@
 
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Page</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
+<p>Hello Bootstrap!</p>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="/bootstrap/js/bootstrap.min.js"></script>
 Enter word
-<form method="get" action="/trans/late" enctype="multipart/form-data; charset=UTF-8">
+<form method="get" action="/trans/late">
     <input type="text" name="word">
     <button type="submit">Return wird!</button>
 </form>
